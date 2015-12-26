c3d23c6684c9205e0d54cc129a86355874260653
David Liechty <qwertyshoe@gmail.com>
2015-12-26 11:35:08 -0800

    Add command line arguments and basic logging.

--- scrape-light-novel.py
+++ scrape-light-novel.py
@@ -2,6 +2,18 @@
 
 import urllib.request
 from bs4 import BeautifulSoup
+import argparse
+import logging
+import platform
+import subprocess
+
+DEFAULT_START_URL = 'http://totobro.com/shen-yin-wang-zuo-chapter-1/'
+DEFAULT_OUTPUT_FILE = 'out.txt'
+DEFAULT_LOG_FILE = 'log.txt'
+
+LOGGING_FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
+
+LOG_LEVEL = logging.DEBUG
 
 # spoof some headers so the request appears to be coming
 # from a browser, not a bot
@@ -10,20 +22,61 @@ headers = {
     '(KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
 }
 
-with open('out.txt', 'w', encoding='utf-8') as writer:
+# Set up command line arguments
+argParser = argparse.ArgumentParser(description='Scrape all blog content '
+                                    'from long-running posts separate by '
+                                    'chapters.')
 
-    url = 'http://totobro.com/shen-yin-wang-zuo-chapter-1/'
+argParser.add_argument('-o', '--outputfile', help='Output File',
+                       default=DEFAULT_OUTPUT_FILE)
+argParser.add_argument('-s', '--site',
+                       help='URL of first chapter to scrape.',
+                       default=DEFAULT_START_URL)
+argParser.add_argument('-l', '--log', help='Log file',
+                       default=DEFAULT_LOG_FILE)
 
-    while True:
-        req = urllib.request.Request(url, headers=headers)
-        html = urllib.request.urlopen(req)
-        bsObj = BeautifulSoup(html.read(), 'html5lib')
-        entry = bsObj.find('div', {'class': 'entry-content'})
 
-        writer.write(str(entry))
+def main():
 
-        nextChapter = bsObj.find('a', href=True, text='Next Chapter')
-        if nextChapter:
-            url = nextChapter['href']
-        else:
-            break
+    args = argParser.parse_args()
+
+    logging.basicConfig(filename=getPath(args.log),
+                        level=LOG_LEVEL, format=LOGGING_FORMAT, filemode='a')
+
+    url = args.site
+
+    logging.info('Writing to output file: [%s]', args.outputfile)
+
+    with open(args.outputfile, 'w', encoding='utf-8') as writer:
+
+        counter = 0
+
+        while True:
+            logging.debug('Retrieving URL: [%s]', url)
+            req = urllib.request.Request(url, headers=headers)
+            html = urllib.request.urlopen(req)
+            bsObj = BeautifulSoup(html.read(), 'html5lib')
+            entry = bsObj.find('div', {'class': 'entry-content'})
+
+            writer.write(str(entry))
+
+            counter += 1
+            if counter % 25 == 0:
+                logging.info('Done with [%i] chapters.', counter)
+
+            nextChapter = bsObj.find('a', href=True, text='Next Chapter')
+            if nextChapter:
+                url = nextChapter['href']
+            else:
+                break
+
+
+def getPath(path):
+    '''Convert path to cygwin format if running on a cygwin platform'''
+
+    if 'CYGWIN' in platform.system():
+        path = subprocess.getoutput('cygpath ' + path)
+    return path
+
+if __name__ == '__main__':
+    main()
