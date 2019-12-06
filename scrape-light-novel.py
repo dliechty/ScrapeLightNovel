f5de47577f47c083b76d0269894dcd4d9a2d9065
David Liechty <qwertyshoe@gmail.com>
2019-12-06 11:43:39 -0800

    Update spacing and formatting. Also convert from camel case to snake case.

--- scrape-light-novel.py
+++ scrape-light-novel.py
@@ -30,13 +30,13 @@ LOG_LEVEL = logging.INFO
 # from a browser, not a bot
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 '
-    '(KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+                  '(KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
 }
 
 # Set up command line arguments
 argParser = argparse.ArgumentParser(description='Scrape all blog content '
-                                    'from long-running posts separate by '
-                                    'chapters.')
+                                                'from long-running posts separate by '
+                                                'chapters.')
 
 argParser.add_argument('-t', '--title',
                        help='Title and base name of output file',
@@ -49,10 +49,9 @@ argParser.add_argument('-l', '--log', help='Log file',
 
 
 def main():
-
     args = argParser.parse_args()
 
-    logging.basicConfig(filename=getPath(args.log),
+    logging.basicConfig(filename=get_path(args.log),
                         level=LOG_LEVEL, format=LOGGING_FORMAT, filemode='a')
 
     url = args.site
@@ -73,41 +72,41 @@ def main():
             logging.debug('Retrieving URL: [%s]', url)
             req = urllib.request.Request(url, headers=headers)
             html = urllib.request.urlopen(req)
-            bsObj = BeautifulSoup(html.read(), 'html5lib')
-            entry = bsObj.find('div', {'class': 'entry-content'})
+            bs_obj = BeautifulSoup(html.read(), 'html5lib')
+            entry = bs_obj.find('div', {'class': 'entry-content'})
 
-            textList = [' '.join(ch.stripped_strings)
-                        for ch in entry if hasattr(ch, 'stripped_strings')]
+            text_list = [' '.join(ch.stripped_strings)
+                         for ch in entry if hasattr(ch, 'stripped_strings')]
 
-            textList = [unidecode.unidecode(s) for s in textList]
+            text_list = [unidecode.unidecode(s) for s in text_list]
 
-            for i in range(0, len(textList)):
+            for i in range(0, len(text_list)):
                 for pattern in BAD_STRINGS:
-                    if re.match(pattern, textList[i]):
-                        textList[i] = ''
+                    if re.match(pattern, text_list[i]):
+                        text_list[i] = ''
 
                 preview = False
-                if (re.match('.*Chapter \d{1,}.*', textList[i])):
-                    if textList[i - 1] == 'Preview:':
-                        textList[i - 1] = ''
+                if re.match('.*Chapter \d{1,}.*', text_list[i]):
+                    if text_list[i - 1] == 'Preview:':
+                        text_list[i - 1] = ''
                         preview = True
 
-                    cleanChapter = re.sub(r'(.*)(Chapter \d{1,}.*)',
-                                          r'\2', textList[i])
-                    textList[i] = ('\n\n<h1 id="ch' + str(counter) + '">' +
-                                   ('Preview: ' if preview else '') +
-                                   cleanChapter + '</h1>')
+                    clean_chapter = re.sub(r'(.*)(Chapter \d{1,}.*)',
+                                           r'\2', text_list[i])
+                    text_list[i] = ('\n\n<h1 id="ch' + str(counter) + '">' +
+                                    ('Preview: ' if preview else '') +
+                                    clean_chapter + '</h1>')
 
-                    chapters.append(('ch' + str(counter), cleanChapter))
+                    chapters.append(('ch' + str(counter), clean_chapter))
 
-                if (re.match('1\.', textList[i])):
-                    textList[i] = '\n' + textList[i]
+                if re.match('1\.', text_list[i]):
+                    text_list[i] = '\n' + text_list[i]
 
-            textList = [('<p>' + s + '</p>' if
-                         not s.startswith('\n\n<h1>Chapter') else s)
-                        for s in textList if s]
+            text_list = [('<p>' + s + '</p>' if
+                          not s.startswith('\n\n<h1>Chapter') else s)
+                         for s in text_list if s]
 
-            text = '\n'.join(textList)
+            text = '\n'.join(text_list)
 
             text += '\n<div class="pagebreak"></div>'
 
@@ -116,15 +115,15 @@ def main():
             if counter % 25 == 0:
                 logging.info('Done with [%i] chapters.', counter)
 
-            nextChapter = bsObj.find('a', href=True, text='Next Chapter')
-            if nextChapter:
-                url = nextChapter['href']
+            next_chapter = bs_obj.find('a', href=True, text='Next Chapter')
+            if next_chapter:
+                url = next_chapter['href']
             else:
                 break
 
         writer.write(FILE_FOOTER)
 
-        writer.write(buildTOC(chapters))
+        writer.write(build_toc(chapters))
         logging.info('Done scraping. Grabbed [%s] chapters.', counter)
 
     logging.info('Renaming file [%s] to [%s].',
@@ -132,25 +131,26 @@ def main():
     move(args.title, args.title + '_' + str(counter) + '.html')
 
 
-def getPath(path):
-    '''Convert path to cygwin format if running on a cygwin platform'''
+def get_path(path):
+    """Convert path to cygwin format if running on a cygwin platform"""
 
     if 'CYGWIN' in platform.system():
         path = subprocess.getoutput('cygpath ' + path)
     return path
 
 
-def buildTOC(chapters):
-    boilerTop = '<div id="toc">\n<h2>\nTable of Contents <br />\n</h2>\n<ul>\n'
-    boilerBot = '</ul>\n</div>\n<div class="pagebreak"></div>'
+def build_toc(chapters):
+    boiler_top = '<div id="toc">\n<h2>\nTable of Contents <br />\n</h2>\n<ul>\n'
+    boiler_bot = '</ul>\n</div>\n<div class="pagebreak"></div>'
 
-    toc = boilerTop
+    toc = boiler_top
 
     for link, text in chapters:
         toc += '<li><a href="#' + link + '">' + text + '</a></li>\n'
 
-    toc += boilerBot
+    toc += boiler_bot
     return toc
+
 
 if __name__ == '__main__':
     main()
